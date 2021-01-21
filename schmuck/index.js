3d2451a25316542be8863ffb498e2b82fe5100ac
malceore <malceore@gmail.com>
2021-01-21 08:03:04 +0800

    potato potato potato

--- schmuck/index.js
+++ schmuck/index.js
@@ -1,21 +1,22 @@
-  let g = hexi(512, 512, setup, load);
+let thingsToLoad = [
+  "res/alien.png",
+];
+
+  let g = hexi(512, 512, setup,  thingsToLoad, load);
   g.fps = 30;
+
+
   //Optionally scale and align the canvas inside the browser window
   g.scaleToWindow();
   g.start();
   let cats, message;
   
   function load() {
-    //Display the file currently being loaded
-    console.log(`loading: ${g.loadingFile}`);
-    //Display the percentage of files currently loaded
-    console.log(`progress: ${g.loadingProgress}`);
-    //Add an optional loading bar.
     g.loadingBar();
   }
 
   function setup() {
-    message = g.text("Tap for cats!", "38px puzzler", "red");
+    message = g.text("Madduck Studios", "38px puzzler", "red");
     g.stage.putCenter(message);
     message.pivotX = 0.5;
     message.pivotY = 0.5;
