4359ea2aadf31ef327447a1960dd5a62054dbdce
malceore <malceore@gmail.com>
2021-01-21 08:11:14 +0800

    added buttons and made it log when clicked, title repaired.

--- schmuck/index.js
+++ schmuck/index.js
@@ -4,7 +4,7 @@ let thingsToLoad = [
 
   let g = hexi(512, 512, setup,  thingsToLoad, load);
   g.fps = 30;
-
+  let version = 0.1;
 
   //Optionally scale and align the canvas inside the browser window
   g.scaleToWindow();
@@ -16,16 +16,25 @@ let thingsToLoad = [
   }
 
   function setup() {
-    message = g.text("Madduck Studios", "38px puzzler", "red");
+    message = g.text("Schmuck " + version, "38px puzzler", "red");
     g.stage.putCenter(message);
     message.pivotX = 0.5;
     message.pivotY = 0.5;
+
+    playButton = g.text("Play", "38px puzzler", "red");
+    playButton.x = 400;
+    playButton.y = 350;
+
+    g.makeInteractive(playButton);
+    playButton.press = () => console.log("butts");
+
     g.state = play;
+
   }
   
   function play() {
     //Rotate the text
-    message.rotation += 0.1;
+    //message.rotation += 0.1;
   }
   
 
