6302725f086e9910c0bf631cf87ba86c958080d6
malceore <malceore@gmail.com>
2021-01-28 08:29:52 +0800

    made start button switch to game scene now, removed butt log.

--- schmuck/index.js
+++ schmuck/index.js
@@ -9,27 +9,34 @@ let thingsToLoad = [
   //Optionally scale and align the canvas inside the browser window
   g.scaleToWindow();
   g.start();
-  let cats, message;
+  let cats, title, ship;
   
   function load() {
     g.loadingBar();
   }
 
   function setup() {
-    message = g.text("Schmuck " + version, "38px puzzler", "red");
-    g.stage.putCenter(message);
-    message.pivotX = 0.5;
-    message.pivotY = 0.5;
+    title = g.text("Schmuck " + version, "38px puzzler", "red");
+    g.stage.putCenter(title);
+    title.pivotX = 0.5;
+    title.pivotY = 0.5;
+
+    ship = g.circle(25, "blue", "black",  5, 750, 400);
 
     playButton = g.text("Play", "38px puzzler", "red");
     playButton.x = 400;
     playButton.y = 350;
 
+    titleScene = g.group(title, playButton);
+    gameScene = g.group(ship);
+
     g.makeInteractive(playButton);
-    playButton.press = () => console.log("butts");
+    playButton.press = function(){
+      g.slide(titleScene, -514, 0, 30, "decelerationCubed");
+      g.slide(gameScene, -514, 0, 30, "decelerationCubed");
+    }
 
     g.state = play;
-
   }
   
   function play() {
